aa720560957ae7f6858c88b4fa2e692cedefddca
Milan <134322921+Mil-555@users.noreply.github.com>
2023-06-20 17:29:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let p=document.getElementById("prz")
-let lewy=document.querySelector(".lewy")
-let prawy=document.querySelector(".prawy")
+let lnapis=document.querySelector(".ln")
+let rnapis=document.querySelector(".rn")
 let losu1
 let losu2
 let losc1
@@ -9,16 +9,18 @@ let pu=0
 let pc=0
 let kostka=0
 function odlicz(liczba){
-    prawy.innerHTML=liczba
-    lewy.innerHTML=liczba
+    lnapis.innerHTML=liczba
+    rnapis.innerHTML=liczba
 }
 function rzut(){
     losu1=parseInt(Math.random()*6)+1
     losu2=parseInt(Math.random()*6)+1
     losc1=parseInt(Math.random()*6)+1
     losc2=parseInt(Math.random()*6)+1
-    lewy.innerHTML=`<p>USER:</p> <br> <p><img src="kostka/${losu1}.jpg">  <img src="kostka/${losu2}.jpg"></p>`
-    prawy.innerHTML=`<p>COMP:</p> <br> <p><img src="kostka/${losc1}.jpg">  <img src="kostka/${losc2}.jpg"></p>`
+    lnapis.innerHTML=`USER:`
+    rnapis.innerHTML=`COMP:`
+    document.querySelector(".lk").innerHTML=`<p><img src="kostka/${losu1}.jpg">  <img src="kostka/${losu2}.jpg"></p>`
+    document.querySelector(".rk").innerHTML=`<p><img src="kostka/${losc1}.jpg">  <img src="kostka/${losc2}.jpg"></p>`
 }
 function win(){
     if((losu1+losu2)>(losc1+losc2)){
@@ -43,8 +45,3 @@ function nazwa(){
     setTimeout(win,3100)
 }
 p.addEventListener("click",nazwa)
-
-// 1.format przycisku rzut w css
-// 2.sprawdź kto wygrał - daj komunikat
-// 3.licznik punktów dla usera i dla kompa + wyświetlanie
-// 4.zmiana numerów na graficzne kostki
